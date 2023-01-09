5a329ea0974648c1a37b80501a458f58d7a1bb3f
Petar D <pdeveric@gmail.com>
2023-01-10 00:17:47 +0100

    news.php update 1

--- paragliding/news.php
+++ paragliding/news.php
@@ -9,29 +9,21 @@
             <?php include_once("navigation.php"); ?>
         </header>
         <main>
-        <h1>Novosti</h1>
         <div id="news-container">
+        <h1>Vijesti</h1>
         <?php 
             $query  = "SELECT * FROM news";
             $query .= " ORDER BY id DESC";
             $result = @mysqli_query($dbConnection, $query);
             $row = @mysqli_fetch_array($result);
-            print '<table><tbody>';
             while($row = @mysqli_fetch_array($result)) {
-                print '
-                <tr>
-                    <td>' . $row['title'] . '</td>
-                    <td>';
-                    if(strlen($row['description']) > 160) {
-                        echo substr(strip_tags($row['description']), 0, 160).'...';
-                    } else {
-                        echo strip_tags($row['description']);
-                    }
-                    print '
-                    </td>
-                </tr>';
+                print '<div class="news">
+                            <h2>'.$row['title'].'</h2>
+                            <img src="images/'.$row['picture'].'"/>
+                            <p style="font-size: 1em;">'.$row['description'].'</p>
+                            <hr/>
+                        </div>';
             }
-            print '</tbody></table>';
         ?>
         </div>
         </main>
